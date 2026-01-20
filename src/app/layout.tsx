f64180f6bd8398b0e5f7221ce339b4c5e2c95291
marukosy124 <soniayeung12435@gmail.com>
2026-01-20 00:30:50 -0500

    feat: clerk for auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Footer from '@/app/_components/footer';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,20 +26,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2811046510872625"
-          crossOrigin="anonymous"
-        ></script>
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen bg-linear-to-br from-gray-50 via-blue-50 to-purple-50`}
-      >
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2811046510872625"
+            crossOrigin="anonymous"
+          ></script>
+        </head>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen bg-linear-to-br from-gray-50 via-blue-50 to-purple-50`}
+        >
+          {children}
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,21 @@
+import { Button } from '@/components/ui/button';
+import {
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  UserButton,
+} from '@clerk/nextjs';
 import { Sparkles } from 'lucide-react';
 import Link from 'next/link';
 
 export default function Header() {
   return (
     <header
-      className="sticky top-0 z-30 backdrop-blur-sm"
+      className="sticky top-0 z-30 backdrop-blur-sm "
       style={{ WebkitBackdropFilter: 'blur(8px)' }}
     >
-      <div className="container mx-5 flex items-center h-15 select-none">
+      <div className="container px-5 flex items-center h-15 select-none justify-between">
         <Link
           href="/"
           className="flex items-center space-x-3 group"
@@ -16,10 +24,33 @@ export default function Header() {
           <span className="inline-flex items-center justify-center w-8 h-8 rounded-full bg-linear-to-br from-blue-500 to-purple-500 shadow-lg transition-transform group-hover:scale-105">
             <Sparkles className="w-4 h-4 text-white" />
           </span>
-          <span className="text-xl md:text-2xl font-bold bg-linear-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent tracking-tight group-hover:underline">
+          <span className="text-xl md:text-2xl font-bold bg-linear-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent tracking-tight ">
             InspoLens
           </span>
         </Link>
+
+        <div>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <Button
+                variant="ghost"
+                className="text-blue-600 hover:text-purple-600 hover:bg-blue-50/50 font-medium transition-colors"
+              >
+                Sign In
+              </Button>
+            </SignInButton>
+            <SignUpButton mode="modal">
+              <Button className="relative overflow-hidden text-white font-medium shadow-md hover:shadow-lg transition-shadow group">
+                <span className="absolute inset-0 bg-linear-to-r from-blue-500 to-purple-500" />
+                <span className="absolute inset-0 bg-linear-to-r from-blue-600 to-purple-600 -translate-x-full group-hover:translate-x-0 transition-transform duration-300 ease-out" />
+                <span className="relative z-10">Sign Up</span>
+              </Button>
+            </SignUpButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
       </div>
     </header>
   );
