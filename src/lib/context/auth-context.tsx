3806d6ef8b0ab6fed58ac3152cc9500f6b20b1d6
marukosy124 <soniayeung12435@gmail.com>
2026-02-26 17:23:43 -0500

    fix: profile not fetched in auth context

--- src/lib/context/auth-context.tsx
+++ src/lib/context/auth-context.tsx
@@ -8,8 +8,9 @@ import {
   ReactNode,
 } from 'react';
 import { supabaseClient } from '@/lib/supabase/client';
-import { CompleteUser, UserProfile } from '@/lib/types';
+import { CompleteUser } from '@/lib/types';
 import { officialUser } from '@/lib/constants';
+import { Session } from '@supabase/supabase-js';
 
 type AuthContextType = {
   user: CompleteUser | null;
@@ -28,37 +29,48 @@ export function AuthProvider({
   const [user, setUser] = useState<CompleteUser | null>(initialUser);
   const [isLoading, setIsLoading] = useState(
     !initialUser && initialUser !== null
-  ); // only loading if we don't have initial data
+  );
 
   useEffect(() => {
-    // Listen for auth changes
+    // get session
+    const init = async () => {
+      setIsLoading(true);
+
+      const {
+        data: { session },
+      } = await supabaseClient.auth.getSession();
+
+      await handleSession(session);
+    };
+
+    // get profile if session exists
+    const handleSession = async (session: Session | null) => {
+      if (!session?.user) {
+        setUser(officialUser);
+        setIsLoading(false);
+        return;
+      }
+
+      const { data: profile } = await supabaseClient
+        .from('profiles')
+        .select('username, avatar_url, avatar_color')
+        .eq('id', session.user.id)
+        .single();
+
+      if (profile) {
+        setUser({ ...session.user, ...profile, is_official: false });
+      }
+
+      setIsLoading(false);
+    };
+
+    // proactively retrieve session on init instead of relying on auth state change events being emitted
+    init();
+
     const {
       data: { subscription },
-    } = supabaseClient.auth.onAuthStateChange(async (_, session) => {
-      let user: CompleteUser | null = null;
-      if (session?.user) {
-        const currentUser = session.user;
-
-        // Get the profile (single row)
-        const { data: profile, error: profileError } = await supabaseClient
-          .from('profiles')
-          .select('username, avatar_url, avatar_color')
-          .eq('id', currentUser.id)
-          .single<UserProfile>();
-
-        if (profileError) {
-          console.error('Profile fetch error:', profileError);
-        }
-
-        if (profile && currentUser) {
-          user = { ...currentUser, ...profile, is_official: false };
-        }
-      } else {
-        // set official user as user otherwise
-        user = officialUser;
-      }
-      setUser(user);
-      setIsLoading(false);
+    } = supabaseClient.auth.onAuthStateChange(async (_event, session) => {
+      await handleSession(session);
     });
 
     return () => subscription.unsubscribe();
